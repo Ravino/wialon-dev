e06d14aabb08cb228399b4101c1b746fec2b1986
nikita.savchuk.2000@gmail.com <45071191+SavchukNikita@users.noreply.github.com>
2019-05-27 13:09:06 +1000

    'test'

--- wialon/test.js
+++ wialon/test.js
@@ -1,5 +1,5 @@
 const wialon = require('./wialon');
 const fs = require("fs");
 let file = fs.readFileSync("data.txt", "utf8");
-console.log(wialon.get(file).lon1);
+console.log(wialon.get(file).imei);
 console.log(wialon.getBus(file,'352848029853261').course);
