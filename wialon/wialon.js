2ea48a58ef20224a54430fad4993f15118b6fe14
nikita.savchuk.2000@gmail.com <45071191+SavchukNikita@users.noreply.github.com>
2019-06-18 20:51:51 +1000

    aster

--- wialon/wialon.js
+++ wialon/wialon.js
@@ -40,4 +40,45 @@ GetCourse: function(str){
       course = s[7]
   };
   return course;
+},
+GetCoordsArray: function(str){
+  str = str.split(os.EOL);
+  let coords = [];
+  j = 0;
+  for (let i=0; i<str.length; i++){
+    s = str[i].split(';');
+    if (s[0][1] == 'D'){
+      coords[j] = {};
+      coords[j].lat1 = s[2];
+      coords[j].lat2 = s[3];
+      coords[j].lon1 = s[4];
+      coords[j].lon2 = s[5];
+      j++;
+    };
+  };
+  return coords;
+},
+GetSpeedArray: function(str){
+  str = str.split(os.EOL);
+  j = 0;
+  speed = [];
+  for (let i=0; i<str.length; i++){
+    s = str[i].split(';');
+    if (s[0][1] == 'D'){
+      speed[j] = s[6];
+      j++;}
+  };
+  return speed;
+},
+GetCourseArray: function(str){
+  str = str.split(os.EOL);
+  j = 0;
+  course = [];
+  for (let i=0; i<str.length; i++){
+    s = str[i].split(';');
+    if (s[0][1] == 'D'){
+      course[j] = s[7];
+      j++;}
+  };
+  return course;
 }};
